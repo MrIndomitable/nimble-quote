6d6d475feb751f6a3dd1c516b25ae671d866ab8a
Menachem Akwa <akwa770@gmail.com>
2017-09-06 13:55:23 +0300

    Add Imminent Table and Offers Table - Does not include real data

--- react-ui/src/components/App/App.js
+++ react-ui/src/components/App/App.js
@@ -6,6 +6,8 @@ import {Header} from '../Header/Header';
 import {QuotesLifeCycle} from '../quotes/LifeCycle/LifeCycle';
 import {NewQuoteWizard} from '../NewQuoteForm/NewQuoteWizard';
 import {PendingQuotesTable} from '../quotes/PendingQuotesTable/PendingQuotesTable';
+import {OffersTable} from '../quotes/OffersTable/OffersTable';
+import {ImminentQuotesTable} from '../quotes/ImminentQuotesTable/ImminentQuotesTable';
 
 const App = () => (
   <div className="App">
@@ -14,6 +16,8 @@ const App = () => (
     <Route path="/quotes" component={QuotesLifeCycle}/>
     <Route path="/quotes/pending" component={PendingQuotesTable}/>
     <Route path="/quotes/create" component={NewQuoteWizard}/>
+    <Route path="/quotes/offers" component={OffersTable}/>
+    <Route path="/quotes/imminent" component={ImminentQuotesTable}/>
   </div>
 );
 
---
+++ react-ui/src/components/quotes/ImminentQuotesTable/ImminentQuotesTable.js
@@ -0,0 +1,57 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {fetchQuotes} from '../../../actions/rfp-actions';
+
+class ImminentQuotesTableClass extends Component {
+  componentWillMount() {
+    this.props.fetchQuotes()
+  }
+
+  render() {
+    return <ImminentQuotesTableComp {...this.props}/>
+  }
+}
+
+const ImminentQuotesTableComp = ({quotes})=>{
+    const ImminentQuotesRows = quotes.map(({id, partNumber, manufacture, quantity, targetPrice, date}) =>{
+        return(
+            <tr key={id}>
+            <td>{manufacture}</td>
+            <td>{partNumber}</td>
+            <td>{quantity}</td>
+            <td>{targetPrice}</td>
+            <td>{date}</td>
+            <td>
+            <button className="btn btn-primary btn-raised btn-sm">Purchase Order</button>
+            </td>
+        </tr>
+        )
+    });
+    return (
+        <table className="table table-striped">
+        <thead>
+        <tr>
+            <th>Manufacture</th>
+            <th>Part #</th>
+            <th>Quantity</th>
+            <th>Target price</th>
+            <th>Date</th>
+        </tr>
+        </thead>
+        <tbody>
+        {ImminentQuotesRows}
+        </tbody>
+        </table>
+    );
+} 
+
+const mapStateToProps = state => ({
+  quotes: state.quotes
+});
+
+const mapDispatchToProps = {
+  fetchQuotes
+};
+
+
+export const ImminentQuotesTable = connect(mapStateToProps, mapDispatchToProps)(ImminentQuotesTableClass);
