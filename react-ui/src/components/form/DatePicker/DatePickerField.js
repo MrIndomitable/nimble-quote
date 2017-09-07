a742591c3cfe226a279ccb8f47e72d4158526228
moshem <moshem@wix.com>
2017-09-07 14:08:26 +0300

    move DatePickerFieldComp outside of it's container - to reduce redundant rendering

--- react-ui/src/components/form/DatePicker/DatePickerField.js
+++ react-ui/src/components/form/DatePicker/DatePickerField.js
@@ -4,28 +4,35 @@ import moment from 'moment';
 import {Field} from 'redux-form';
 import 'react-datepicker/dist/react-datepicker.css';
 
-export const DatePickerField = ({id, placeholder}) => {
-	const dateFormat = 'DD-MM-YYYY';
 
-	const DatePickerFieldComp = ({input}) => {
-		const handleChange = (date) => {
-			input.onChange(moment(date).format(dateFormat))
-		}
-		return (
-			<div className="form-group">
-				<DatePicker
-					{...input}
-					placeholderText={placeholder}
-					className="form-control"
-					dateFormat={dateFormat}
-					selected={input.value ? moment(input.value, dateFormat) : null}
-					onChange={handleChange}
-					label={placeholder}
-					/>
-			</div>
-		)
-	}
-	return (
-		<Field name={id} component={DatePickerFieldComp} />
-	)
-}
+const DatePickerFieldComp = ({input, placeholder}) => {
+  const dateFormat = 'DD-MM-YYYY';
+
+  const handleChange = (date) => {
+    input.onChange(moment(date).format(dateFormat))
+  };
+
+  return (
+    <div className="form-group">
+      <DatePicker
+        {...input}
+        placeholderText={placeholder}
+        className="form-control"
+        dateFormat={dateFormat}
+        selected={input.value ? moment(input.value, dateFormat) : null}
+        onChange={handleChange}
+        label={placeholder}
+      />
+    </div>
+  )
+};
+
+export const DatePickerField = ({id, placeholder}) => {
+  return (
+    <Field
+      name={id}
+      placeholder={placeholder}
+      component={DatePickerFieldComp}
+    />
+  );
+};
