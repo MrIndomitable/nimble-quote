bcf45615c9699a7c32013dcd69bd4ff0ad33d4ae
Menachem Akwa <akwa770@gmail.com>
2017-09-12 11:49:38 +0300

    Create Single component page

--- react-ui/src/components/App/App.js
+++ react-ui/src/components/App/App.js
@@ -5,12 +5,15 @@ import {Redirect} from 'react-router-dom';
 import {Header} from '../Header/Header';
 import {NewQuoteWizard} from '../NewQuoteForm/NewQuoteWizard';
 import {ComponentsPage} from '../quotes/ComponentsPage/ComponentsPage';
+import {SingleComponent} from '../quotes/SingleComponent/SingleComponent';
 
+console.log(SingleComponent);
 const App = () => (
   <div className="App">
     <Header/>
     <Route exact path="/" render={() => <Redirect to="/components"/>}/>
     <Route path="/components/:filter?" component={ComponentsPage}/>
+    <Route path="/single-component" component={SingleComponent}/>
     <Route path="/bom/create" component={NewQuoteWizard}/>
   </div>
 );
