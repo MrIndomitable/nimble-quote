629115fe3b1d755564cf026071f2e21999ca77ce
moshem <moshem@wix.com>
2017-09-13 22:57:59 +0300

    refactor PendingSuppliersTable

--- react-ui/src/components/quotes/SingleComponent/PendingSuppliersTable.js
+++ react-ui/src/components/quotes/SingleComponent/PendingSuppliersTable.js
@@ -1,46 +1,29 @@
 import React from 'react';
 
-const SupplierRow = ({supplier}) => {
+const SupplierRow = ({email, sentDate, status}) => {
   return (
     <tr>
-      <td>{supplier.email}</td>
-      <td>{supplier.review}</td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td>{supplier.sentDate}</td>
-      <td>{supplier.status}</td>
+      <td>{email}</td>
+      <td>{sentDate}</td>
+      <td>{status}</td>
     </tr>
   )
 };
 
 export const PendingSuppliersTable = ({suppliers}) => {
-  const supplierRows = suppliers.map(supplier => {
-    return (
-      <SupplierRow key={supplier.email} supplier={supplier}/>
-    )
-  });
+  const supplierRows = suppliers.map(supplier => <SupplierRow key={supplier.email} {...supplier}/>);
 
   return (
     <table className="table text-center">
       <thead>
       <tr>
         <th className="text-center">Supplier</th>
-        <th className="text-center">Review
-        </th>
-        <th className="text-center"></th>
-        <th className="text-center"></th>
-        <th className="text-center"></th>
-        <th className="text-center"></th>
         <th className="text-center">Date</th>
         <th className="text-center">Status</th>
       </tr>
       </thead>
       <tbody>
-
       {supplierRows}
-
       </tbody>
     </table>
   )
