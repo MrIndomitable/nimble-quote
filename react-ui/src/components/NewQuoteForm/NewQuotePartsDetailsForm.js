6e4340a45b8f6027666e05c518d41d4a38e5190d
Menachem Akwa <akwa770@gmail.com>
2017-10-01 19:51:20 +0300

    fix layout of new quote first page

--- react-ui/src/components/NewQuoteForm/NewQuotePartsDetailsForm.js
+++ react-ui/src/components/NewQuoteForm/NewQuotePartsDetailsForm.js
@@ -12,25 +12,27 @@ const QuoteDetails = ({name}) => {
     <InputField id={`${name}.targetPrice`} placeholder="Target price" type="number"/>    
     <DatePickerField id={`${name}.partDate`} placeholder="Part date" type="date" />
     <DatePickerField id={`${name}.supplyDate`} placeholder="Supply date" type="date" />
-
+    <div>$12.00</div>
+<hr/>
   </div>
 };
 
 const renderQuotes = ({fields}) => {
-  return <div>
+  return <div className="new-quote-fields-container">
     {fields.map((quote, i) => {
       return <div key={i}><QuoteDetails name={quote}/></div>
     })}
-    <button className="btn btn-default" type="button" onClick={() => fields.push({})}>
+    <button className="btn btn-default add-new-line" type="button" onClick={() => fields.push({})}>
       <span className="fa fa-plus"/> Add new
     </button>
+    <hr/>
   </div>
 };
 
 export const NewQuotePartsDetailsFormComp = ({handleSubmit}) => {
-  return <form onSubmit={ handleSubmit }>
+  return <form className="new-quote-form" onSubmit={ handleSubmit }>
     <FieldArray name="details" component={renderQuotes}/>
-    <button type="submit" className="btn btn-success btn-lg">
+    <button type="submit" className="btn btn-success btn-lg choose-supplier">
       Choose supplier's <span className="fa fa-caret-right"/>
     </button>
   </form>
