4342e726caa4d5bfb9143dc2ba8580550512d733
Menachem Akwa <akwa770@gmail.com>
2017-10-01 18:58:50 +0300

    Fix checkbox issue in purchase order form

--- react-ui/src/components/form/InputField.js
+++ react-ui/src/components/form/InputField.js
@@ -2,12 +2,11 @@ import React from 'react';
 import {Field} from 'redux-form';
 
 const renderField = ({id, input, label, type, placeholder, className, meta: { touched, error, warning } }) => (
-  <div>
+<div className="form-group">
       {label && <label htmlFor={id}>{label}</label>}
-    <div>
+    
       <input {...input} placeholder={placeholder} type={type} className={className}/>
       {touched && ((error && <span className="error">{error}</span>) || (warning && <span className="warning">{warning}</span>))}
-    </div>
   </div>
 )
 
@@ -15,7 +14,7 @@ const renderField = ({id, input, label, type, placeholder, className, meta: { to
 export const InputField = ({id, label, type, placeholder, validate}) => {
 
   return (
-    <div className="form-group">
+    
       <Field
         id={id}
         className="form-control"
@@ -26,6 +25,6 @@ export const InputField = ({id, label, type, placeholder, validate}) => {
         label={label}
         validate={validate}
       />
-    </div>
+    
   )
 };
