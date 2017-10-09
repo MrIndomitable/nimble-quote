1143c3230f0a36b1a52dd6a7812c59d817fca929
Menachem Akwa <akwa770@gmail.com>
2017-10-09 07:11:46 +0300

    fix layout of new quote second page

--- react-ui/src/components/NewQuoteForm/NewQuoteSupplierDetailsForm.js
+++ react-ui/src/components/NewQuoteForm/NewQuoteSupplierDetailsForm.js
@@ -3,17 +3,18 @@ import {InputField} from '../form/InputField';
 import {NewQuoteHOC} from './NewQuoteWizardConfig';
 
 export const NewQuoteSupplierDetailsFormComp = ({handleSubmit, pristine, previousPage, submitting}) => {
-  return <form onSubmit={ handleSubmit }>
+  return <form className="new-quote-form-supplier" onSubmit={ handleSubmit }>
     <InputField id="supplier" label="Supplier" type="email"/>
     <InputField id="subject" label="Subject" type="text"/>
-    <InputField id="date" label="Date" type="text"/>
     <InputField id="message" label="Message" type="text"/>
+   <div className="form-group btns-wrapper"> 
     <button className="btn btn-default btn-lg" onClick={previousPage}>
       <span className="fa fa-caret-left"/> Back
     </button>
-    <button type="submit" className="btn btn-success btn-lg" disabled={pristine || submitting}>
+    <button type="submit" className="btn btn-success btn-lg send-quote-btn" disabled={pristine || submitting}>
       Send Quote <span className="fa fa-caret-right"/>
     </button>
+    </div>
   </form>
 };
 
