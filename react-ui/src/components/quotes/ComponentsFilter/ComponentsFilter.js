7ec0fa65e9855d23d9baa398075e4f1808537648
Moshe Matsliah <moshem@wix.com>
2017-10-13 01:04:15 +0300

    make components table filter clickable

--- react-ui/src/components/quotes/ComponentsFilter/ComponentsFilter.js
+++ react-ui/src/components/quotes/ComponentsFilter/ComponentsFilter.js
@@ -3,17 +3,24 @@ import {NavLink} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {sumComponents} from '../../../selectors/components-selector';
 
-const ComponentsFilterComp = ({offersBadge}) => {
-  return <div className="arrow-progress-bar-container"><ul className="arrow-progress-bar">
-    <li><NavLink to="/components?q=pending">Pending</NavLink></li>
-    <li><NavLink to="/components?q=offers">
-      Offers {offersBadge > 0 && <span className="badge"> {offersBadge}</span>}
-    </NavLink></li>
-    <li><NavLink to="/components?q=imminent">Imminent</NavLink></li>
-    <li><NavLink to="/components?q=archive">Archive</NavLink></li>
-  </ul>
-  </div>;
-};
+const FilterLink = ({label, filter, children}) => (
+  <NavLink to={`/components?q=${filter}`}>
+    <li>{label || children}</li>
+  </NavLink>
+);
+
+const ComponentsFilterComp = ({offersBadge}) => (
+  <div className="arrow-progress-bar-container">
+    <ul className="arrow-progress-bar">
+      <FilterLink label="Pending" filter="pending"/>
+      <FilterLink filter="offers">
+        Offers {offersBadge > 0 && <span className="badge"> {offersBadge}</span>}
+      </FilterLink>
+      <FilterLink label="Imminent" filter="imminent"/>
+      <FilterLink label="Archive" filter="archive"/>
+    </ul>
+  </div>
+);
 
 const mapStateToProps = state => ({
   offersBadge: sumComponents(state, 'offers')
