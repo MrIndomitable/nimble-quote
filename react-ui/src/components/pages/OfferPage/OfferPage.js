b19722c4882b66bc6966cf34523f46f871d5bad1
Moshe Matsliah <moshem@wix.com>
2017-10-15 11:56:43 +0300

    add 'thank you' message and styling to add offer flow

--- react-ui/src/components/pages/OfferPage/OfferPage.js
+++ react-ui/src/components/pages/OfferPage/OfferPage.js
@@ -5,6 +5,10 @@ import {parse} from 'query-string';
 
 export class OfferPage extends React.Component {
   state = {
+    loading: true,
+    submitting: false,
+    submitted: false,
+    hasErrors: false,
     auction: null
   };
 
@@ -14,31 +18,59 @@ export class OfferPage extends React.Component {
       token
     };
     axios.get('/api/offer', {params})
-      .then(res => this.setState({auction: res.data}))
+      .then(res => this.setState({auction: res.data, loading: false}))
       .catch(e => console.error(e));
   }
 
-  render() {
-    const submitOffer = offerDetails => {
-      console.log('submit offer', offerDetails);
-      const token = parse(this.props.location.search).t;
-      axios.post('/api/offer', {offerDetails, token})
-        .then(res => console.log('offer receive'))
-        .catch(e => console.log(e));
-    };
+  submitOffer(offerDetails) {
+    this.setState({submitting: true});
+    const token = parse(this.props.location.search).t;
+    axios.post('/api/offer', {offerDetails, token})
+      .then(() => this.setState({submitting: false, submitted: true}))
+      .catch(e => this.setState({submitting: false, hasErrors: true}));
+  };
 
-    const {auction} = this.state;
+  render() {
+    return <div className="container">
+      {this.getCurrentView()}
+    </div>;
+  }
+
+  getCurrentView() {
+    const {auction, loading, submitting, submitted, hasErrors} = this.state;
+
+    if (loading) {
+      return <i className="fa fa-spinner fa-spin fa-5x fa-fw"/>;
+    }
+
+    if (submitting) {
+      return <div className="alert" role="alert">
+        <h2>Submitting offer, please wait...</h2>
+      </div>;
+    }
+
+    if (submitted) {
+      return <div className="alert alert-success" role="alert">
+        <h2>Your offer is being processed and viewed, thanks you!</h2>
+      </div>;
+    }
+
+    if (hasErrors) {
+      return <div className="alert alert-danger" role="alert">
+        <span className="glyphicon glyphicon-exclamation-sign" aria-hidden="true"/>
+        <span className="sr-only">Error:</span> An error occurred, please try again later
+      </div>;
+    }
 
     if (auction) {
       return <div>
         <h2>Please insert your offer</h2>
-        <NewOfferTable auction={auction} submitOffer={submitOffer}/>
-      </div>
+        <NewOfferTable auction={auction} submitOffer={offer => this.submitOffer(offer)}/>
+      </div>;
     }
 
-    return <div>
+    return <div className="alert alert-warning" role="alert">
       <h2>Auction not found</h2>
-      <i className="fa fa-spinner fa-spin fa-5x fa-fw"/>
     </div>
   }
 }
