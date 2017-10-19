1b4f1301eaa66b6ccf51d48ae930366b66f32193
Menachem Akwa <akwa770@gmail.com>
2017-10-19 17:57:58 +0300

    fix a to link

--- react-ui/src/components/AppLogo/AppLogo.js
+++ react-ui/src/components/AppLogo/AppLogo.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import logo from '../../../public/logo.png';
 import './AppLogo.css';
+import {Link} from 'react-router-dom';
 
 export const AppLogo = () => {
   return (
     <div className="app-logo">
       <h2>Nimble Quote</h2>
-      <a href="/">
-      <img src={logo} className="App-logo" alt="logo"/>
-      </a>
+      <Link  to="/">
+        <img src={logo} className="App-logo" alt="logo"/>
+      </Link>
     </div>
   );
 };
