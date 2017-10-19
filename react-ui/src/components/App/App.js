5faff87c862e849e6cb1d77411b986e6d139a47b
Menachem Akwa <akwa770@gmail.com>
2017-10-19 17:57:25 +0300

    add redirect to login page

--- react-ui/src/components/App/App.js
+++ react-ui/src/components/App/App.js
@@ -8,22 +8,8 @@ import {PurchaseOrderWizard} from '../PurchaseOrderForm/PurchaseOrderWizard';
 import {ComponentsPage} from '../quotes/ComponentsPage/ComponentsPage';
 import {SingleComponent} from '../quotes/SingleComponent/SingleComponent';
 import {OfferPage} from '../pages/OfferPage/OfferPage';
-
+import {ProtectedRoute} from '../routes/ProtectedRoute';
 import {Login} from '../routes/Login/Login';
-// import Auth from '../routes/Auth';
-
-// const PrivateRoute = ({ component: Component, ...rest }) => (
-//   <Route {...rest} render={props => (
-//     fakeAuth.isAuthenticated ? (
-//       <Component {...props}/>
-//     ) : (
-//       <Redirect to={{
-//         pathname: '/login',
-//         state: { from: props.location }
-//       }}/>
-//     )
-//   )}/>
-// )
 
 const App = () => (
   <div className="App">
@@ -31,7 +17,7 @@ const App = () => (
 
     <Route exact path="/login" component={Login} />
     <Route exact path="/" render={() => <Redirect to="/components?q=pending"/>}/>
-    <Route exact path="/components" component={ComponentsPage}/>
+    <ProtectedRoute exact path="/components" component={ComponentsPage}/>
     <Route path="/components/:id" component={SingleComponent}/>
     <Route path="/bom/create" component={NewQuoteWizard}/>
     <Route path="/purchase-order/create" component={PurchaseOrderWizard}/>
---
+++ react-ui/src/components/routes/ProtectedRoute.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Route} from 'react-router';
+import {Redirect} from 'react-router-dom';
+import {connect} from 'react-redux';
+
+const ProtectedRouteComp = ({ isAuthenticated, component: Component, ...rest }) => (
+  <Route {...rest} render={props => (
+    isAuthenticated ? (
+      <Component {...props}/>
+    ) : (
+      <Redirect to={{
+        pathname: '/login',
+        state: { from: props.location }
+      }}/>
+    )
+  )}/>
+)
+
+const mapStateToProps = ({user})=>{
+    return {isAuthenticated: user.isLoggedIn}
+}
+export const ProtectedRoute = connect(mapStateToProps)(ProtectedRouteComp);
