f9070960ed54d68d85f8ee63e3db42ae6bdad9a3
Menachem Akwa <akwa770@gmail.com>
2017-10-21 21:20:59 +0300

    maek the login form with redux

--- react-ui/src/components/routes/Login/Login.jsx
+++ react-ui/src/components/routes/Login/Login.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-// import axios from 'axios';
-// import {parse} from 'query-string';
+import {reduxForm} from 'redux-form';
+import {InputField} from '../../form/InputField';
 import axios from 'axios';
 import {
   BrowserRouter as Router,
@@ -12,36 +12,32 @@ import {
 import {AuthButton} from '../../SignInButton/SignInButton';
 import './login.css';
 
-export class Login extends React.Component {
-  // state = {
-  //   auth: false
-  // };
-render(){
-  
-  return(
-    <div className = "container">
-      <div className="wrapper login-wrapper">
-        <form action="" method="post" name="Login_Form" className="form-signin">       
-            <h3 className="form-signin-heading">Please Login</h3>
-            
-            <input type="text" className="form-control" name="Username" placeholder="Username" required="" autofocus="" /><br/>
-            <input type="password" className="form-control" name="Password" placeholder="Password" required=""/>     		  
-          
-            <button
-              onClick={
-                e => {
-                  e.preventDefault();
-                  axios.post('/auth/signup', {email: 'abc', password: '123'})
-                    .then(res => console.log('signed up, just reload'))
-                    .catch(e => console.log('error while signing up', e));
-                }
-              }
-              className="btn btn-lg btn-primary btn-block"  name="Submit" value="Login" type="Submit">Login</button>
-        </form>			
-      <AuthButton/>
+export const LoginComp = () => {
+  return <div  className = "container">
+    <div className="wrapper login-wrapper"><form className="form-signin">
+      <h3 className="form-signin-heading">Please Login</h3>
+      <InputField id="username" label="Username" placeholder="Username" type="text"/>
+      <InputField id="password" label="Password" placeholder="Password" type="password"/>
+      <div className="form-group btns-wrapper">
+        <button
+          onClick={
+            e => {
+              e.preventDefault();
+              axios.post('/auth/signup', { email: 'abc', password: '123' })
+                .then(res => console.log('signed up, just reload'))
+                .catch(e => console.log('error while signing up', e));
+            }
+          }
+          className="btn btn-lg btn-primary btn-block"  name="Submit" value="Login" type="Submit">Login</button>
       </div>
+    </form>
+      <AuthButton/>
     </div>
-  );
+  </div>
 };
 
-};
+export const Login = reduxForm({
+  form: 'LoginForm',
+  destroyOnUnmount: true,
+})(LoginComp);
+
