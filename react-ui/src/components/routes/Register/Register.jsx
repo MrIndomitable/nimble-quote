ab33a0d5d277ea99aff05fc2c10ecfc8b0db6267
Menachem Akwa <akwa770@gmail.com>
2017-10-24 00:32:11 +0300

    add validation

--- react-ui/src/components/routes/Register/Register.jsx
+++ react-ui/src/components/routes/Register/Register.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import {reduxForm} from 'redux-form';
 import {InputField} from '../../form/InputField';
 import './register.css';
+import * as v from '../../form/Validation/Validation';
 
 const RegisterFormComp = ({handleSubmit}) => (
   <form className="form-signup">
     <h3 className="form-signup-heading">Welcome to Nimble Quote</h3>
-    <InputField id="email" label="Email" placeholder="Email" type="text"/>
-    <InputField id="password" label="Password" placeholder="Password" type="password"/>
-    <InputField id="confirm-password" label="Confirm Password" placeholder="Confirm Password" type="password"/>
+    <InputField id="email" label="Email" placeholder="Email" type="text" validate={[v.required]}/>
+    <InputField id="password" label="Password" placeholder="Password" type="password" validate={[v.required]}/>
+    <InputField id="confirm-password" label="Confirm Password" placeholder="Confirm Password" type="password" validate={[v.required]}/>
     <div className="form-group btns-wrapper">
       <button
         onClick={handleSubmit}
