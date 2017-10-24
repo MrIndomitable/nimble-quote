53de569bfef27e31c3a7eda6f48e931cd76e1969
Menachem Akwa <akwa770@gmail.com>
2017-10-24 23:35:59 +0300

    fix acknowledge action

--- react-ui/src/components/pages/ViewOrderPage/ViewOrderPage.jsx
+++ react-ui/src/components/pages/ViewOrderPage/ViewOrderPage.jsx
@@ -5,9 +5,32 @@ import axios from 'axios';
 import {Link} from 'react-router-dom';
 import './ViewOrderPage.css';
 
+const Alert = ({submitted, hasErrors}) => {
+  if (submitted) {
+    return <div className="invoice-container">
+      <div className="alert alert-success" role="alert">
+        <h2>Your Approveing PO is being processed and viewed, thanks you!</h2>
+      </div>
+    </div>
+  }
+  if (hasErrors) {
+    return <div className="invoice-container">
+      <div className="alert alert-danger" role="alert">
+        <h2>Ther is a problem during Approveing PO!</h2>
+      </div>
+    </div>
+  }
+  return null
+
+}
+
+
+
 export class ViewOrderPage extends React.Component {
   state = {
-    order: null
+    order: null,
+    submitted: false,
+    hasErrors: false
   };
 
   componentWillMount() {
@@ -18,21 +41,34 @@ export class ViewOrderPage extends React.Component {
   }
 
   render() {
+    console.log(this.state);
     const {order} = this.state;
+    const {submitted, hasErrors} = this.state;
 
+    const acknowledge = () => {
+      axios.post(`/api/acknowledge`, { token: order })
+        .then(() => this.setState({ submitted: true }))
+        .catch(e => this.setState({ hasErrors: true }));
+      // .then(res => console.log({ order: res.data }))
+      // .catch(e => console.log(e));
+    }
+    const AcknowledgeButton = ({acknowledge}) => {
+      return <nav className="navbar navbar-light bg-light">
+        <button className="btn btn-lg btn-primary pull-right" type="button" onClick={acknowledge} >Acknowledge</button>
+      </nav>
+    }
     if (order) {
       return <div className="container">
-        <nav className="navbar navbar-light bg-light">
-          <button className="btn btn-lg btn-primary pull-right" type="button">Acknowledge</button>
-        </nav>
+
+        <AcknowledgeButton acknowledge={acknowledge}/>
+        <Alert {...this.state}/>
         <div className="invoice-container">
           <Invoice {...order}/>;
         </div>
-        <nav className="navbar navbar-light bg-light">
-          <button className="btn btn-lg btn-primary pull-right" type="button">Acknowledge</button>
-        </nav>
+        <AcknowledgeButton acknowledge={acknowledge}/>
       </div>
     }
+
 
     return <h3>No order found</h3>;
   }
